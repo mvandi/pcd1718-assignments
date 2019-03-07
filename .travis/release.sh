8e4ee2267267663e9a9c1d8dd093dc43eb00e88f
Mattia Vandi <mattia.vandi@studio.unibo.it>
2019-03-07 09:46:52 +0100

    Modified release script to accept more than one assignment and added multi-project support

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 set -e
 
-if [ "$#" != "1" ]; then
+if [ "$#" == "0" ]; then
     echo "Usage: $0 <assignment>" >& 2
     exit 1
 fi
 
 cwd=`pwd`
-assignment=$1
+assignments=$@
 root=`git rev-parse --show-toplevel`
 releaseDir=$root/release
 
-cd $root/$assignment
+for assignment in $assignments; do
+    cd $root/$assignment
 
-# Copy documentation
-mkdir -p $releaseDir/$assignment/doc
-cp doc/report/report.pdf $releaseDir/$assignment/doc
+    # Copy documentation
+    mkdir -p $releaseDir/$assignment/doc
+    cp doc/report/report.pdf $releaseDir/$assignment/doc
 
-# Copy sources, scripts and gradle-related files
-cp -R src gradle build.gradle gradle.properties settings.gradle gradlew* $releaseDir/$assignment || :
+    # Copy sources, scripts and gradle-related files
+    cp -R src gradle build.gradle gradle.properties settings.gradle gradlew* $releaseDir/$assignment || :
 
-cd $releaseDir
+    cd $releaseDir
 
-# Make the zip
-zip -r $assignment.zip $assignment > /dev/null
-rm -r $assignment
+    # Make the zip
+    zip -r $assignment.zip $assignment > /dev/null
+    rm -r $assignment
+done
 
 cd $cwd
--- Assignment-02/src/main/java/pcd/ass02/ex1/tasks/FolderSearchTask.java
+++ Assignment-02/src/main/java/pcd/ass02/ex1/tasks/FolderSearchTask.java
@@ -44,4 +44,3 @@ class FolderSearchTask extends RecursiveTask<Long> {
     }
 
 }
-    
